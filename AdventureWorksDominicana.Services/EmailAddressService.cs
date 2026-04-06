75dd11ffa36895586633c3b2569ed27c53d4b745
Jeremy Sanchez Cabrera <jeremy_sanchez@ucne.edu.do>
2026-04-06 16:35:29 -0400

    Agrega el Service incompleto de EmailAddress.

---
+++ AdventureWorksDominicana.Services/EmailAddressService.cs
@@ -0,0 +1,41 @@
+﻿using AdventureWorksDominicana.Data.Context;
+using AdventureWorksDominicana.Data.Models;
+using Aplicada1.Core;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace AdventureWorksDominicana.Services;
+
+public class EmailAddressService(IDbContextFactory<Contexto> DbFactory) : IService<EmailAddress, int>
+{
+    public async Task<EmailAddress?> Buscar(int id)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.EmailAddresses.Include(b => b.BusinessEntity).ThenInclude(c => c.Customers).FirstOrDefaultAsync(e => e.EmailAddressId == id);
+    }
+
+    public async Task<EmailAddress?> BuscarPorEmail(string email)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.EmailAddresses.Include(b => b.BusinessEntity).ThenInclude(c => c.Customers).FirstOrDefaultAsync(e => e.EmailAddress1.Trim().ToLower().Equals(email));
+    }
+
+    public Task<bool> Eliminar(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<List<EmailAddress>> GetList(Expression<Func<EmailAddress, bool>> criterio)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.EmailAddresses.Where(criterio).AsNoTracking().ToListAsync();
+    }
+
+    public Task<bool> Guardar(EmailAddress entidad)
+    {
+        throw new NotImplementedException();
+    }
+}
